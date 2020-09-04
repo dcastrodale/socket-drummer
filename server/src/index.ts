67c3a6abf28fc551418852620993c9413598dbdf
David Castrodale <dcastrodale@gmail.com>
2020-09-04 15:11:09 +0100

    Update gitignore file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello world!');
+const Ws = require('ws');
+
+const wss = new Ws.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
+  ws.send('something');
+});
