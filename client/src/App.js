6d6e3a8b26ebc0403cb2ebecd7187153d6a12c2e
David Castrodale <dcastrodale@gmail.com>
2020-09-06 18:48:33 +0100

    Send and receive messages from client

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,57 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [socketStatus, setSocketStatus] = useState('CLOSED');
+  const [message, setMessage] = useState(null);
+  const socketRef = useRef({ socket: null });
+
+  const handleMessage = (msg) => {
+    setMessage(msg);
+  };
+
+  useEffect(() => {
+    const connectToWebsocket = async () => {
+      socketRef.current.socket = new WebSocket('ws://localhost:8080');
+      const { socket } = socketRef.current;
+
+      socket.addEventListener('open', () => {
+        console.log('connection open');
+        setSocketStatus('OPEN');
+      });
+
+      socket.addEventListener('close', () => {
+        console.log('Closing socket');
+        setSocketStatus('CLOSED');
+      });
+
+      socket.addEventListener('message', (event) => {
+        handleMessage(event.data);
+      });
+    };
+
+    if (socketStatus === 'CLOSED') {
+      connectToWebsocket();
+    }
+
+    return () => {
+      socketStatus === 'OPEN' && socketRef.current.close();
+    };
+  }, [socketStatus]);
+
+  const handleClick = () => {
+    if (socketStatus === 'OPEN') {
+      const { socket } = socketRef.current;
+      socket.send('Click event');
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {message && <h1>{message}</h1>}
+        <button onClick={handleClick}>Click me</button>
       </header>
     </div>
   );
